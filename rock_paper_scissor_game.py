343d80d5865b985e390924fe9ef652faccaf260d
Arafat Islam <islamarafat377@gmail.com>
2026-03-17 01:03:36 +0600

    Add files via upload

---
+++ rock_paper_scissor_game.py
@@ -0,0 +1,27 @@
+import random
+item_list = ["Rock", "Paper","Scissor"]
+
+user_choice = input("Enter your move : Rock, Paper, Scissor=")
+comp_choice = random.choice(item_list)
+
+print(f"User choice = {user_choice},Computer choice = {comp_choice}")
+
+if user_choice == comp_choice:
+    print("Both chooses same: = Match Tie")
+
+elif user_choice == "Rock":
+    if comp_choice == "Paper":
+        print("Paper covers Rock = Computer Win")
+    else:
+        print("Rock smashes Scissor = You Win")
+
+elif user_choice == "Paper":
+    if comp_choice == "Scissor":
+        print("Scissor cuts paper = Computer Win")
+    else:
+        print("Paper covers Rock = You Win")
+elif user_choice == "Scissor":
+    if comp_choice == "Paper":
+        print("Scissor cuts paper = You Win")
+    else:
+        print("Rock smashes Scissor = Computer Win")
